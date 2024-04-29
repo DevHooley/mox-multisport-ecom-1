210b3d479f077ec92d20c1b46b5e9a71eb5ae723
Your Name <92824573+kyleehooley@users.noreply.github.com>
2024-04-29 17:17:59 -0500

    latest

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,26 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        lightTheme: {
+          primary: "#6CC9FB",
+          secondary: "#181E2A",
+          accent: "#1be885",
+          neutral: "#272136",
+          "base-100": "#ffffff",
+          info: "#778ad4",
+          success: "#23b893",
+          warning: "#f79926",
+          error: "#ea535a",
+          body: {
+            "background-color": "#FBE0F1",
+          },
+        },
       },
-    },
+    ],
   },
-  plugins: [],
 };
 export default config;
